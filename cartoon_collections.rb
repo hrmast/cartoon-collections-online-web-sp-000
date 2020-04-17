bc28712ff8475697418729589daf373a5e97bd3a
Heath Mast <hrmast@gmail.com>
2020-04-17 02:06:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types)
-    return cheese_types
+    return cheese_types[0]
   else
     nil
   end
